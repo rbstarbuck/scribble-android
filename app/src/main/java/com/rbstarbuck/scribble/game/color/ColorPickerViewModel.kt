01e2406f30276a4fbf8a382c3aad1dbefbef5898
rbstarbuck <rbstarbuck@gmail.com>
2024-12-18 23:05:03 -0500

    Add basic color picker view

---
+++ app/src/main/java/com/rbstarbuck/scribble/game/color/ColorPickerViewModel.kt
@@ -0,0 +1,9 @@
+package com.rbstarbuck.scribble.game.color
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class ColorPickerViewModel: ViewModel() {
+    val hueStateFlow = MutableStateFlow(180f)
+    val saturationValueStateFlow = MutableStateFlow(0f to 0f)
+}
