27c14d502752b9a0af9ecce1011b2b879ff2bdd1
rbstarbuck <rbstarbuck@gmail.com>
2024-12-31 17:06:38 -0500

    Maintain proper sizing of drawn bounding box during/after rotation

--- app/src/main/java/com/rbstarbuck/scribble/game/transform/RotateView.kt
+++ app/src/main/java/com/rbstarbuck/scribble/game/transform/RotateView.kt
@@ -2,14 +2,13 @@ package com.rbstarbuck.scribble.game.transform
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
@@ -22,7 +21,6 @@ fun RotateView(
     modifier: Modifier = Modifier
 ) {
     val selectedLayer by viewModel.selectedLayerStateFlow.collectAsState()
-    val strokesRotation by selectedLayer.strokes.rotationStateFlow.collectAsState()
 
     val localRotationStateFlow = remember { MutableStateFlow(0f) }
     val localRotation by localRotationStateFlow.collectAsState()
@@ -34,24 +32,27 @@ fun RotateView(
     val recomposeBoundingBox by recomposeBoundingBoxStateFlow.collectAsState()
 
     key(recomposeBoundingBox) {
-        Box(
+        Canvas(
             modifier = modifier
                 .graphicsLayer {
-                    transformOrigin = TransformOrigin(bounds.center.x, bounds.center.y)
+                    transformOrigin = TransformOrigin(
+                        pivotFractionX = bounds.center.x / size.width,
+                        pivotFractionY = bounds.center.y / size.height
+                    )
                     rotationZ = localRotation
                 }
                 .pointerInput(Unit) {
                     detectDragGestures(
-                        onDragStart = {
-                            boundsStateFlow.value = selectedLayer.strokes.boundingBox()
-                        },
                         onDrag = { change, offset ->
                             val degrees = (offset.x / size.width + offset.y / size.height) * 180f
 
                             localRotationStateFlow.value += degrees
                             selectedLayer.strokes.rotateZ(
                                 degrees = degrees,
-                                strokesCenter = bounds.center
+                                strokesCenter = Offset(
+                                    x = bounds.center.x / size.width,
+                                    y = bounds.center.y / size.height
+                                )
                             )
                         },
                         onDragEnd = {
@@ -60,21 +61,8 @@ fun RotateView(
                         }
                     )
                 }
-        )
-    }
-
-    key(strokesRotation) {
-        Canvas(
-            modifier = modifier
-                .fillMaxSize()
-                .graphicsLayer {
-                    boundsStateFlow.value = selectedLayer.strokes.boundingBox()
-
-                    transformOrigin = TransformOrigin(bounds.center.x, bounds.center.y)
-                    rotationZ = strokesRotation
-                }
         ) {
-            drawTransformBox(selectedLayer.strokes)
+            boundsStateFlow.value = drawTransformBox(selectedLayer.strokes)
         }
     }
 }
--- app/src/main/java/com/rbstarbuck/scribble/game/transform/Transform.kt
+++ app/src/main/java/com/rbstarbuck/scribble/game/transform/Transform.kt
@@ -1,5 +1,6 @@
 package com.rbstarbuck.scribble.game.transform
 
+import android.graphics.Matrix
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
@@ -18,9 +19,10 @@ enum class TransformType {
 
 fun DrawScope.drawTransformBox(
     strokes: Strokes,
-    scale: Offset = Offset.Zero
+    scale: Offset = Offset.Zero,
+    rotation: Float = 0f
 ): Rect {
-    val bounds = strokesBoundingBox(strokes, scale)
+    val bounds = strokesBoundingBox(strokes, scale, rotation)
     val boundsPadding = 16.dp.toPx()
 
     val topLeft = Offset(
@@ -48,12 +50,15 @@ fun DrawScope.drawTransformBox(
 
 private fun DrawScope.strokesBoundingBox(
     strokes: Strokes,
-    scale: Offset = Offset.Zero
+    scale: Offset,
+    rotation: Float
 ): Rect {
     var left = Float.MAX_VALUE
     var right = Float.MIN_VALUE
     var top = Float.MAX_VALUE
     var bottom = Float.MIN_VALUE
+
+    var totalPoints = 0
 
     for (stroke in strokes.committedStrokes) {
         if (stroke.brushType != BrushType.Eraser) {
@@ -65,14 +70,54 @@ private fun DrawScope.strokesBoundingBox(
                 if (horizontal > right) right = horizontal
                 if (vertical < top) top = vertical
                 if (vertical > bottom) bottom = vertical
+
+                totalPoints += 2
             }
         }
     }
 
+    val center = Rect(left, top, right, bottom).center
+
+    val dstArray = FloatArray(totalPoints)
+    val srcArray = FloatArray(totalPoints)
+
+    var i = 0
+    for (stroke in strokes.committedStrokes) {
+        if (stroke.brushType != BrushType.Eraser) {
+            for (point in stroke.points) {
+                srcArray[i++] = point.x
+                srcArray[i++] = point.y
+            }
+        }
+    }
+
+    val matrix = Matrix()
+    matrix.postRotate(-rotation, center.x, center.y)
+    matrix.mapPoints(dstArray, srcArray)
+
+    left = Float.MAX_VALUE
+    right = Float.MIN_VALUE
+    top = Float.MAX_VALUE
+    bottom = Float.MIN_VALUE
+
+    for (i in 0..<totalPoints / 2) {
+        val xIndex = i * 2
+        val yIndex = xIndex + 1
+
+        if (dstArray[xIndex] < left) left = dstArray[xIndex]
+        if (dstArray[xIndex] > right) right = dstArray[xIndex]
+        if (dstArray[yIndex] < top) top = dstArray[yIndex]
+        if (dstArray[yIndex] > bottom) bottom = dstArray[yIndex]
+    }
+
     return Rect(
-        left = left * size.width - scale.x,
-        top = top * size.height - scale.y,
-        right = right * size.width + scale.x,
-        bottom = bottom * size.height + scale.y
+        topLeft = Offset(
+            x = left * size.width - scale.x,
+            y = top * size.height - scale.y
+        ),
+        bottomRight = Offset(
+            x = right * size.width + scale.x,
+            y = bottom * size.height + scale.y
+        )
     )
 }
